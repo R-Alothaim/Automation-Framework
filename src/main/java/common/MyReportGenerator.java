6762018746e46eae1e733c84f57750a222f785df
Ralothaim <ralothaim@outlook.com>
2024-01-09 13:11:44 +0300

    reporting

---
+++ src/main/java/common/MyReportGenerator.java
@@ -0,0 +1,17 @@
+package common;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+public class MyReportGenerator {
+	public static ExtentReports getReport() {
+		String path = System.getProperty("user.dir")+"\\reportHTML\\index.html";
+		ExtentSparkReporter reporter = new ExtentSparkReporter(path);
+		reporter.config().setReportName("Web Automation Results");
+		reporter.config().setDocumentTitle("Test Result");
+		ExtentReports extent = new ExtentReports();
+		extent.attachReporter(reporter);
+		extent.setSystemInfo("Tester", "Ryan");
+		return extent;
+	}
+}
